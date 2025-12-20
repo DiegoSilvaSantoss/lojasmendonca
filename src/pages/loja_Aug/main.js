4e9f38380965e6a24d08e2bb8f0887e430654dc5
Dhii Santos <diegoalphapro@gmail.com>
2025-12-20 15:05:02 -0300

    Reajsutes

--- src/pages/loja_Aug/main.js
+++ src/pages/loja_Aug/main.js
@@ -1,5 +1,7 @@
 import addressAug from "./services/addressAug.js";
 import rootPageVendedor from "./page/vendedores/services/rootPage.js";
+import rootPageService from "./page/atendimento/services/rootPageService.js";
 
 addressAug()
 rootPageVendedor()
+rootPageService()
