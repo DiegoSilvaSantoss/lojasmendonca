2145b2f30f46810681bff5e675e00ab33b2e49ba
Dhii Santos <diegoalphapro@gmail.com>
2025-12-22 23:37:53 -0300

    Reajustes

--- src/pages/loja_ann/services/addressAug.js
+++ src/pages/loja_ann/services/addressAug.js
@@ -1,10 +1,10 @@
-/*export default function addressAug() {
+export default function addressAug() {
     const btn = document.getElementById('address');
 
     if (!btn) return;
 
     btn.addEventListener('click', () => {
-        window.open('https://maps.app.goo.gl/mqgJ3NHwE39zaj2U9', '_blank')
+        window.open('https://maps.app.goo.gl/HBhLsfNp5hpN39Kj7', '_blank')
     });
-}*/
+}
 
